27cf62b754d4e6b034dfb875570ba52cffe035ba
JulianDeco <102333273+JulianDeco@users.noreply.github.com>
2022-10-11 22:34:38 -0300

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
